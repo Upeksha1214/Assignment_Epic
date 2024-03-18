223900556acb592d5f71b689772ee5e7380cf89e
Upeksha Kularathna <upeksha_k@epic.lk>
2024-03-18 17:02:55 +0530

    model calss is updated

--- src/java/com/mycompany/assingment/model/User.java
+++ src/java/com/mycompany/assingment/model/User.java
@@ -9,67 +9,16 @@ package com.mycompany.assingment.model;
  * @author upeksha_k
  */
 public class User {
-    private String userId;
-    private String name;
-    private String email;
-    private int phoneNumber;
-    private String password;
+    private String employeeCode;
+    private String firstName;
+    private String lastName;
+    private int Address;
+    private String NIC;
+    private int mobileNo;
+    private String gender;
+    private String eMail;
+    private String designationMasterData;
+    private String dateOfBirth;
+    private String status; 
     
-    public User(String userId, String name, String email, int phoneNumber) {
-        this.userId = userId;
-        this.name = name;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public User() {
-    }
-
-    public User(String userId, String name, String email, int phoneNumber, String password) {
-        this.userId = userId;
-        this.name = name;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-        this.password = password;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(int phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
